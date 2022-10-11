3372f2aac06818793e8cc5c3f8a898a3a5c23b78
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-11 17:52:26 +0900

    refactor: try with resources 로 수정

--- src/main/java/presentation/MainPrompt.java
+++ src/main/java/presentation/MainPrompt.java
@@ -19,24 +19,21 @@ public class MainPrompt {
     private final ControllerFacade controllerFacade;
 
     public void start() {
-        BufferedReader commandReader = new BufferedReader(new InputStreamReader(System.in));
-
-        while (Status.getCurStatus() != StatusType.EXIT) {
-            try {
+        try (BufferedReader commandReader = new BufferedReader(new InputStreamReader(System.in))) {
+            while (Status.getCurStatus() != StatusType.EXIT) {
                 String commandline = commandReader.readLine();
                 String[] parsed = commandParser.parse(commandline);
                 String command = commandParser.getCommand(parsed);
                 String[] args = commandParser.getArguments(parsed);
                 controllerFacade.execute(command, args);
-
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } catch (CommandException e) {
-                System.out.println("명령어 잘못됐다는 메시지");
-            } catch (ArgumentException e) {
-                System.out.println("인자 잘못됐다는 메시지");
             }
-
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (CommandException e) {
+            System.out.println("명령어 잘못됐다는 메시지");
+        } catch (ArgumentException e) {
+            System.out.println("인자 잘못됐다는 메시지");
         }
+
     }
 }
