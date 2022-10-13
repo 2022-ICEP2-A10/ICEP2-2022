c9b82f0f3361070aa7d6f6c3c8d68a0133d11d28
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-13 11:48:57 +0900

    feat: 회원 repository

---
+++ src/main/java/repository/MemberRepository.java
@@ -0,0 +1,28 @@
+package repository;
+
+import domain.Member;
+import lombok.RequiredArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class MemberRepository {
+
+    private final Map<String, Member> members;
+
+    public Optional<Member> findById(String id) {
+        return Optional.ofNullable(members.get(id));
+    }
+
+    public void save(Member member) {
+        members.put(member.getUserid(), member);
+    }
+
+    public List<Member> findAll() {
+        return new ArrayList<>(members.values());
+    }
+
+}
