28fee60e6865478b43940ee1b81251d410d33367
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-19 10:41:34 +0900

    fix: LocalDateTime 문자열 변환

--- src/main/java/domain/Member.java
+++ src/main/java/domain/Member.java
@@ -2,10 +2,10 @@ package domain;
 
 import lombok.Builder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * Member 클래스 빌더 패턴 사용 예시
@@ -29,6 +29,10 @@ public class Member {
 
     @Override
     public String toString() {
-        return userid + "\t" + password + "\t" + possible + "\t" + possibleDate + "\t" + userType;
+        String date = null;
+        if (possibleDate != null) {
+            date = possibleDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        }
+        return userid + "\t" + password + "\t" + possible + "\t" + date + "\t" + userType;
     }
 }
