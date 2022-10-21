8ce585e1bb945385dd5ab12cd98b91b8d8fe2824
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-21 22:36:46 +0900

    fix: try catch 위치 변경

--- src/main/java/presentation/MainPrompt.java
+++ src/main/java/presentation/MainPrompt.java
@@ -19,20 +19,20 @@ public class MainPrompt {
     private final ControllerFacade controllerFacade;
 
     public void start() {
-        while (Status.getCurStatus() != StatusType.EXIT) {
-            try (BufferedReader commandReader = new BufferedReader(new InputStreamReader(System.in))) {
-                String commandline = commandReader.readLine();
-                String[] parsed = commandParser.parse(commandline);
-                String command = commandParser.getCommand(parsed);
-                String[] args = commandParser.getArguments(parsed);
-                controllerFacade.execute(command, args);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            } catch (CommandException e) {
-                System.out.println("명령어 잘못됐다는 메시지");
-            } catch (ArgumentException e) {
-                System.out.println("인자 잘못됐다는 메시지");
+        try (BufferedReader commandReader = new BufferedReader(new InputStreamReader(System.in))) {
+            while (Status.getCurStatus() != StatusType.EXIT) {
+                try {
+                    String commandline = commandReader.readLine();
+                    String[] parsed = commandParser.parse(commandline);
+                    String command = commandParser.getCommand(parsed);
+                    String[] args = commandParser.getArguments(parsed);
+                    controllerFacade.execute(command, args);
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
             }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
 
     }
