180fb8a685543679bc5e3500431ea57e17cc972d
kyuhyeongkwon <akcps1tp@naver.com>
2022-10-23 23:44:49 +0900

    fix: 날짜 출력 오류 수정

--- src/main/java/service/MyLoanService.java
+++ src/main/java/service/MyLoanService.java
@@ -7,6 +7,7 @@ import domain.Book;
 import domain.Checkout;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
 
@@ -22,13 +23,14 @@ public class MyLoanService {
 		 List<Checkout> checkout = checkoutRepository.findAllByUserid(CurrentMember.getCurrentMember().getUserid());
 		 
 		 System.out.println("현재 대출 중인 도서의 목록입니다.");
-		 
+		 DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+
 		 for(Checkout c:checkout) {
 			 Optional<Book> book = bookRepository.findById(c.getBookid());
 			 
 			 LocalDateTime d = c.getCheckoutDate().plusDays(7);
 			 String str = c.getBookid() + " " + book.get().getTitle() + "\n"
-			 +"반납기한: "+ d.getYear() + d.getMonthValue() + d.getDayOfMonth();
+					 + "반납기한: " + d.format(dateTimeFormatter);
 			 
 			 System.out.println(str);		 
 		 }
