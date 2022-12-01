8d284cc99905b99bc617794118d99fd3560351d2
kyuhyeongkwon <akcps1tp@naver.com>
2022-12-02 00:54:41 +0900

    feat: Reserve 도메인 toString 오버라이딩

--- src/main/java/domain/Reserve.java
+++ src/main/java/domain/Reserve.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Builder
 @Getter
@@ -14,4 +15,13 @@ public class Reserve {
     private Long bookid;
     private String userid;
     private LocalDateTime reservedDate;
+
+    @Override
+    public String toString() {
+        String date = null;
+        if (reservedDate != null) {
+            date = reservedDate.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+        }
+        return bookid + "\t" + userid + "\t" + date;
+    }
 }
