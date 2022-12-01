f96832890a0bd9bd7ff68b37166f84e67431ccf5
kyuhyeongkwon <akcps1tp@naver.com>
2022-12-02 00:55:44 +0900

    feat: 예약 Repository on destroy 쓰기 로직 추가

--- src/main/java/repository/ReserveRepository.java
+++ src/main/java/repository/ReserveRepository.java
@@ -1,13 +1,19 @@
 package repository;
 
+import domain.Member;
 import domain.Reserve;
 import lombok.RequiredArgsConstructor;
 
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.Map;
 import java.util.Optional;
 
 @RequiredArgsConstructor
-public class ReserveRepository {
+public class ReserveRepository implements FileBaseDatabase {
 
     private final Map<Long, Reserve> reserves;
 
@@ -21,5 +27,18 @@ public class ReserveRepository {
 
     public void deleteById(long bookid) {
         reserves.remove(bookid);
+    }
+
+    @Override
+    public void destroy() {
+        try (BufferedWriter writer = new BufferedWriter(new OutputStreamWriter(
+                new FileOutputStream("./data/reserves"), StandardCharsets.UTF_8))) {
+            for (Reserve reserve : reserves.values()) {
+                writer.write(reserve.toString());
+                writer.newLine();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
