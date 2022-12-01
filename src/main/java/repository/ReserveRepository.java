fc6a23a850e4d4cd8d030adb338942167aa3662b
kyuhyeongkwon <akcps1tp@naver.com>
2022-12-02 00:03:33 +0900

    feat: 예약 검색

---
+++ src/main/java/repository/ReserveRepository.java
@@ -0,0 +1,17 @@
+package repository;
+
+import domain.Reserve;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class ReserveRepository {
+
+    private final Map<Long, Reserve> reserves;
+
+    public Optional<Reserve> findById(long bookid) {
+        return Optional.ofNullable(reserves.get(bookid));
+    }
+}
